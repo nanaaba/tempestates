a52428ee45ea847ba2c4ea0415d06b0a0fed5c3c
naby <abaodum@gmail.com>
2018-06-13 16:16:13 +0100

    Payment Errors fixed

---
+++ public/js/currenttenants.js
@@ -0,0 +1,112 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var datatable = $('#tenantTbl').DataTable({
+    responsive: true,
+    language: {
+        paginate:
+                {previous: "&laquo;", next: "&raquo;"},
+        search: "_INPUT_",
+        searchPlaceholder: "Search…"
+    },
+    order: [[0, "asc"]]
+});
+
+
+getTenants();
+
+function getTenants()
+{
+
+    $.ajax({
+        url: 'getcurrents',
+        type: "GET",
+        dataType: "json",
+        success: function (data) {
+
+            console.log(data);
+            datatable.clear().draw();
+
+
+            if (data.length == 0) {
+                console.log("NO DATA!");
+            } else {
+                $.each(data, function (key, value) {
+
+
+                    var j = -1;
+                    var r = new Array();
+                    // represent columns as array
+                    r[++j] = '<td class="subject">' + value.name + '</td>';
+                    r[++j] = '<td class="subject">' + value.period +' months </td>';
+                    r[++j] = '<td class="subject">' + value.apartment_name+'</td>';
+                    r[++j] = '<td class="subject">' +value.apartment_type +'</td>';
+                    r[++j] = '<td class="subject">' +value.currency+' ' +value.amount +'</td>';
+
+                    r[++j] = '<td><button onclick="editApartment(\'' + value.tenant_code + '\')"  class="btn btn-outline-info btn-sm editBtn" type="button">Edit</button>\n\
+                              <button onclick="deleteTenant(\'' + value.id + '\',\'' + value.name + '\')"  class="btn btn-outline-danger btn-sm deleteBtn" type="button">Delete</button></td>';
+
+                    rowNode = datatable.row.add(r);
+                });
+
+                rowNode.draw().node();
+            }
+
+        },
+        error: function (jXHR, textStatus, errorThrown) {
+            alert(errorThrown);
+        }
+    });
+}
+
+
+function editApartment(code){
+    window.location = "information/"+code;
+}
+
+
+function deleteTenant(code, title) {
+    console.log(code + title);
+    $('#code').val(code);
+    $('#holdername').html(title);
+    $('#confirmModal').modal('show');
+}
+
+$('#deleteForm').on('submit', function (e) {
+    e.preventDefault();
+    $('input:submit').attr("disabled", true);
+    var code = $('#code').val();
+    var token = $('#token').val();
+    $('#confirmModal').modal('hide');
+    $('#loaderModal').modal('show');
+
+    $.ajax({
+        url: code,
+        type: "DELETE",
+        data: {_token: token},
+        success: function (data) {
+            console.log(data);
+            // $("#loader").hide();
+            $('input:submit').attr("disabled", false);
+            $('#loaderModal').modal('hide');
+
+            document.getElementById("deleteForm").reset();
+            if (data == 0) {
+                swal("Success!", "Deleted Successfully", "success");
+                getServices();
+            } else {
+                swal("Error!", "Couldnt delete", "error");
+            }
+        },
+        error: function (jXHR, textStatus, errorThrown) {
+            $('#loaderModal').modal('hide');
+
+            alert(errorThrown);
+        }
+    });
+
+});
